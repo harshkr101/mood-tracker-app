e6646337a165856a3264c6eb9ae2c3f6c10ddaac
Harsh Kumar <harsh.mca19.du@gmail.com>
2023-02-27 15:57:39 +0530

    adds bottom navigator

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
+import {BottomTabNavigator} from 'screens/BottomTabs.navigator';
 
 export const App: React.FC = () => {
-  return <View style={styles.container} />;
+  return (
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'teal',
+    backgroundColor: 'blue',
   },
 });
